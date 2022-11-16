3d79f7edf617caada584777fc666fe30974e9c04
ajeetchaulgain <chaulagainajeet@gmail.com>
2022-11-16 23:55:55 +1100

    add a preview

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="content">
-        <button class="add-cart" @click="addToCart">Add to cart</button>
+        <div class="preview">
+      <div class="preview-content">
+        <div class="top-row">
+          <img :src="selectedRobot.head.src"/>
+        </div>
+        <div class="middle-row">
+          <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+          <img :src="selectedRobot.torso.src"/>
+          <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+        </div>
+        <div class="bottom-row">
+          <img :src="selectedRobot.base.src"/>
+        </div>
+      </div>
+      <button class="add-cart" @click="addToCart">Add to cart</button>
+    </div>
+
         <div class="top-row">
                             <!-- <div class="robot-name">
                     // Interpolation expression
@@ -213,10 +229,10 @@ export default {
 }
 .add-cart {
     position:absolute;
-    right:30px;
-    width:220px;
+    width:210px;
     padding:3px;
     font-size:16px;
+    margin-top:5px;
 }
 
 td,th{
@@ -230,5 +246,27 @@ td,th{
 
 .sale-border{
     border:3px solid red;
+}
+
+.preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+}
+.preview-content {
+  border: 1px solid #999;
+}
+.preview img {
+  width: 50px;
+  height: 50px;
+}
+.rotate-right {
+  transform: rotate(90deg);
+}
+.rotate-left {
+  transform: rotate(-90deg);
 }
 </style>
