9969468f03dcf7be6ea3b78ccaa4adc4f31d5854
ajeetchaulgain <chaulagainajeet@gmail.com>
2022-11-17 15:42:44 +1100

    using nested child routes for browse parts route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
 import PartInfo from '../parts/PartInfo.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotBases from '../parts/RobotBases.vue';
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -15,6 +20,33 @@ export default createRouter({
       path: '/build',
       name: 'Build',
       component: RobotBuilder,
+    },
+    {
+      path: '/parts/browse',
+      name: 'BrowseParts',
+      component: BrowseParts,
+      children: [
+        {
+          name: 'BrowseHeads',
+          path: 'heads',
+          component: RobotHeads,
+        },
+        {
+          name: 'BrowseArms',
+          path: 'arms',
+          component: RobotArms,
+        },
+        {
+          name: 'BrowseTorsos',
+          path: 'torsos',
+          component: RobotTorsos,
+        },
+        {
+          name: 'BrowseBases',
+          path: 'bases',
+          component: RobotBases,
+        },
+      ],
     },
     {
       path: '/parts/:partType/:id',
